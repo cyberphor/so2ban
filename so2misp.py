6bb15c2b70571944f0e82d6b15ccd716cedb26fb
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-08-15 20:17:59 -0700

    Update so2misp.py

--- so2misp.py
+++ so2misp.py
@@ -11,7 +11,45 @@ elastic_api_key = ""
 misp_server_ip = ""
 misp_api_key = ""
 
-def get_events(elastic_server_ip, elastic_server_port, elastic_api_key):
+class Event():
+    date = ""
+    description = ""
+    distribution = {
+        "your_organisation_only": 0,
+	"this_community_only": 1,
+	"connected_communities": 2,
+	"all_communities": 3,
+	"sharing_group": 4,
+	"inherit": 5
+    }
+    threat_level = {
+        "high": 1,
+	"medium": 2,
+	"low": 3,
+	"undefined": 4
+    }
+    analysis = {
+        "initial": 0,
+	"ongoing": 1,
+        "completed": 2
+    }
+
+class Attribute():
+    type = ""
+    category = ""
+    value = ""
+    distribution = {
+        "your_organisation_only": 0,
+	"this_community_only": 1,
+	"connected_communities": 2,
+	"all_communities": 3,
+	"sharing_group": 4,
+	"inherit": 5
+    }
+    for_ids = "false"
+    contextual_comment = ""
+
+def get_cases(elastic_server_ip, elastic_server_port, elastic_api_key):
     elastic_server = elastic_server_ip + ":" + elastic_server_port
     authorization = "ApiKey " + elastic_api_key
     url = "https://" + elastic_server + "/so-case/_search?pretty"
@@ -40,29 +78,11 @@ def get_events(elastic_server_ip, elastic_server_port, elastic_api_key):
                     foo[detail] = case["_source"]["so_case"][detail]
                 for field in update["fields"]:
                     foo[field] = update["fields"][field]
-                pprint(foo)
+                #pprint(foo)
     return
 
-def send_event(misp_server_ip, misp_api_key, event):
-    event = {
-        "Event":{
-            "date":"2020-08-14",
-            "distribution":3,    # all communities
-            "threat_level_id":1, # general
-            "analysis":1,        # ongoing
-            "info":"Demo",
-            "Attribute": [
-                {
-                    "type":"domain",
-                    "category":"Network activity",
-                    "value":"https://cyberphor.com",
-                    "distribution":3,
-                    "to_ids":"false",
-                    "comment":"Known bad domain"
-	        }
-	    ]
-	}
-    }
+def add_event(misp_server_ip, misp_api_key, event):
+    event = Event()
     url = "https://" + misp_server_ip + "/events"
     headers = {
         "Accept": "application/json",
@@ -73,16 +93,19 @@ def send_event(misp_server_ip, misp_api_key, event):
     print(response.text)
     return
 
+def add_attribute():
+    return
+
 def main():
     requests.packages.urllib3.disable_warnings()
     parser = argparse.ArgumentParser()
-    parser.add_argument("--get-events", action="store_true", help = "Get events from Elasticsearch")
-    parser.add_argument("--send-event", action="store_true", help = "Send an event to MISP")
+    parser.add_argument("--get-cases", action="store_true", help = "Get events from Elasticsearch")
+    parser.add_argument("--add-event", action="store_true", help = "Send an event to MISP")
     args = parser.parse_args()
-    if args.get_events:
-        get_events(elastic_server_ip, elastic_server_port, elastic_api_key)
-    elif args.send_event:
-        send_event(misp_server_ip, misp_api_key)
+    if args.get_cases:
+        get_cases(elastic_server_ip, elastic_server_port, elastic_api_key)
+    elif args.add_event:
+        add_event(misp_server_ip, misp_api_key)
     else:
         parser.print_help()
 
