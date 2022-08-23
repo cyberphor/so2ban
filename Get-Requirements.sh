2644633a197520d43d43d7e52244e6bfd7772467
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-08-23 08:29:30 -0700

    Update Get-Requirements.sh

--- Get-Requirements.sh
+++ Get-Requirements.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-wget https://github.com/ktbyers/netmiko/archive/refs/tags/v4.0.0.tar.gz -O netmiko.tar.gz
-gunzip netmiko.tar.gz
-tar -xf netmiko.tar
-cd netmiko 
+wget https://github.com/ktbyers/netmiko/archive/refs/tags/v4.0.0.tar.gz
+gunzip v4.0.0.tar.gz
+tar -xf v4.0.0.tar
+mv netmiko-4.0.0 netmiko
+cd netmiko
 pip3 download -r requirements.txt --no-binary=:all: -d dependencies
