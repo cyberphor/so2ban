b10f9a91d1db01a7c088ac663ddf0cac5685702c
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-09-04 10:36:41 -0700

    Update so2ban.py

--- so2ban.py
+++ so2ban.py
@@ -1,48 +1,33 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 
 import argparse
 import http.server
 import ipaddress
 import json
-#import netmiko
+import netmiko
 import os
-import shutil
 import ssl
 import subprocess
 
-class RequestHandler(object):
-    def __init__(self, address, http.server.BaseHTTPRequestHandler):
-        self.gatekeeper = self.Gateway()
-    class Gateway:
-        def __init__(self):
-            self.settings = {
-                "gatekeeper_type": "",
-                "host": "",
-                "username": "",
-                "password": "",
-            }
-            self.acl = ""
-            self.configure_acl_command_prefix = ""
-            self.block_command_prefix = ""
-            self.unblock_command_prefix = ""
-        def block_host(self, host):
-            commands = [
-                " ".join(self.configure_acl_command_prefix, self.acl), 
-                " ".join(self.block_command_prefix, host)
-            ]
-            #with netmiko.ConnectHandler(**self.settings) as connection:
-            #    connection.send_config_set(commands)
-            #    message = "Blocking " + host + "\n"
-            #return message
-        def unblock_host(self, host):
-            commands = [
-                " ".join(self.configure_acl_command_prefix, self.acl),
-                " ".join(self.unblock_command_prefix, host)
-            ]
-            #with netmiko.ConnectHandler(**self.settings) as connection:
-            #    connection.send_config_set(commands)
-            #    message = "Unblocking " + host + "\n"
-            #return message
+class RequestHandler(http.server.BaseHTTPRequestHandler):
+    settings = {
+        "device_type": "cisco_ios",
+        "host": "192.168.1.1",
+        "username": "admin",
+        "password": "password",
+    }
+    acl_name = "BLOCK_ADVERSARY"
+    configure_acl_command_prefix = "ip access-list standard"
+    block_command_prefix = "1 deny"
+    def block_host(self, host):
+        commands = [
+            (self.configure_acl_command_prefix + " " + self.acl_name), 
+            (self.block_command_prefix + " " + host)
+        ]
+        with netmiko.ConnectHandler(**self.settings) as connection:
+            connection.send_config_set(commands)
+            message = "Blocking " + host + "\n"
+        return "Blocking " + host + "\n"
     def do_GET(self):
         self.send_error(405)
     def do_POST(self):
@@ -53,18 +38,7 @@ class RequestHandler(object):
                 self.send_response(200)
                 self.send_header("Content-Type", "text/html")
                 self.end_headers()
-                #message = self.gatekeeper.block_host(host)
-                self.wfile.write(message.encode("UTF-8"))
-            except ValueError:
-                self.send_error(400)
-        elif self.path.startswith("/unblock/"):
-            host = self.path.split("/unblock/")[1]
-            try:
-                ipaddress.ip_address(host)
-                self.send_response(200)
-                self.send_header("Content-Type", "text/html")
-                self.end_headers()
-                #message = self.gatekeeper.unblock_host(host)
+                message = self.block_host(host)
                 self.wfile.write(message.encode("UTF-8"))
             except ValueError:
                 self.send_error(400)
@@ -74,14 +48,8 @@ class RequestHandler(object):
 def start_listening_api():
     address = ("127.0.0.1", 8666)
     handler = RequestHandler
-    handler.gatekeeper.settings["device_type"] = "cisco_ios"
-    handler.gatekeeper.settings["host"] = "192.168.1.1"
-    handler.gatekeeper.settings["username"] = "admin"
-    handler.gatekeeper.settings["password"] = "password"
-    handler.gatekeeper.acl_name = "BLOCK_ADVERSARY"
-    handler.gatekeeper.acl_command_prefix = "ip access-list standard"
-    handler.gatekeeper.ace_command_prefix = "1 deny"
     api = http.server.HTTPServer(address, handler)
+    """
     api.socket = ssl.wrap_socket(
         api.socket,
         server_side = True,
@@ -89,6 +57,7 @@ def start_listening_api():
         certfile = "public.key",
         ssl_version = ssl.PROTOCOL_TLS
     )
+    """
     api.serve_forever()
     return
 
